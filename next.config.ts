1ebc4e358e210e441aae814c5b4921db4967a18e
Benediktas <benediktas.trilikauskas@code.berlin>
2026-03-25 11:09:44 +0100

    bundeling

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    outputFileTracingIncludes: {
+      "/api/*": ["./app/generated/prisma/**/*"],
+    },
+  }, .
 };
 
 export default nextConfig;
